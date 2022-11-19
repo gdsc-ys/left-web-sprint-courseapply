813a93f6190d887477ed701241b504499004e48e
namooplus <namoo.developer@gmail.com>
2022-11-19 17:48:15 +0900

    fix: fixtures

--- src/data/examples/index.tsx
+++ src/data/examples/index.tsx
@@ -1,27 +1,31 @@
-type classTime = Array<{ day: string; start: string; end: string }>;
+import { Course } from '@/interfaces/Course';
+import { DayOfWeek } from '@/interfaces/Date';
 
-type CourseData = {
-  college: string;
-  courseName: string;
-  courseCode: string;
-  professor: string;
-  //음...
-  time: classTime;
-  classRoom: string;
-  personel: number;
-  credit: number; //학점
-};
-export const courseExample: CourseData = {
-  college: '공과대학',
-  courseName: '데이터베이스',
-  courseCode: 'ABCDEf',
-  professor: '강민서',
-  //걍 Object로 관리할까요? ex [["화" , {start : 1, end : 2}], ["수" , {start : 1, end : 2}]]
-  time: [
-    { day: '화', start: '2', end: '4' },
-    { day: '수', start: '2', end: '4' },
-  ],
-  classRoom: '공A999',
-  personel: 2000,
-  credit: 3, //학점
-};
+export const courses: Course[] = [
+  {
+    id: 'CSI1',
+    college: '공과대학',
+    name: '데이터베이스',
+    professor: '윤강석',
+    times: [
+      { dayOfWeek: DayOfWeek.MONDAY, startPeriod: 2, endPeriod: 4 },
+      { dayOfWeek: DayOfWeek.WEDNESDAY, startPeriod: 2, endPeriod: 4 },
+    ],
+    classroom: '공A999',
+    personnel: 20,
+    credit: 3,
+  },
+  {
+    id: 'CSI2',
+    college: '공과대학',
+    name: '웹',
+    professor: '윤강석',
+    times: [
+      { dayOfWeek: DayOfWeek.MONDAY, startPeriod: 2, endPeriod: 4 },
+      { dayOfWeek: DayOfWeek.WEDNESDAY, startPeriod: 2, endPeriod: 4 },
+    ],
+    classroom: '공A999',
+    personnel: 20,
+    credit: 3,
+  },
+];
