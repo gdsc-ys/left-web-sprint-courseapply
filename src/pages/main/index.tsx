27aeb36914e6f0d9414d3b545f708cd0c75a0109
namooplus <namoo.developer@gmail.com>
2022-11-19 17:58:17 +0900

    feat: clean up code

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,42 +1,38 @@
 import '@pages/main/index.css';
 
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
+import { Course } from '@/interfaces/Course';
 import Basket from '@components/basket';
 import Courses from '@components/courses';
 import Filter from '@components/filter';
 import Header from '@components/header';
-import { courseExample } from '@data/examples';
-import { dataServices } from '@services/dataServices';
-import { CourseData } from '@type/index';
-
-async function getData(
-  setLoadingToGetData: (value: boolean) => void,
-  setCourseData: (value: Array<CourseData>) => void,
-) {
-  setLoadingToGetData(true);
-  try {
-    const response: any = await dataServices.getData();
-    setCourseData(response);
-  } catch (e) {
-    Error();
-  } finally {
-    setLoadingToGetData(false);
-  }
-}
+import { courses as courseExample } from '@data/examples';
 
 export default function Main() {
-  const [courseData, setCourseData] = useState<Array<CourseData>>([
-    courseExample,
-  ]);
-  const [loadingToGetData, setLoadingToGetData] = useState<boolean>(false);
-  const [loadingToApply, setLoadingToApply] = useState<boolean>(false);
+  const [courseData, setCourseData] = useState<Course[]>();
+  // const [loadingToGetData, setLoadingToGetData] = useState<boolean>(false);
+  // const [loadingToApply, setLoadingToApply] = useState<boolean>(false);
+
+  const getCourses = useCallback(async () => {
+    setCourseData(undefined);
+
+    const courses = courseExample;
+
+    setCourseData(courses);
+  }, []);
+
+  useEffect(() => {
+    getCourses();
+  }, []);
+
   return (
     <div>
       <Header />
       <Filter />
       <Courses />
       <Basket />
+      {!courseData && <div>loading...</div>}
     </div>
   );
 }
