25513eafb38160bf9704982d32a87c2fb1e7a6e7
namooplus <namoo.developer@gmail.com>
2022-11-26 22:15:18 +0900

    feat: add api handler examples

--- src/apis/course.ts
+++ src/apis/course.ts
@@ -8,12 +8,20 @@ export interface GetCoursesRequest
 
 export interface GetCoursesResponse extends Array<Course> {}
 
+// GET '/course'
+// 해당 API 호출이 필요한 분이 구현해주세요.
+export const getCourses = () => null;
+
 /**
  * 강의 하나 가져오기
  */
 export interface GetCourseRequest extends Pick<Course, 'id'> {}
 
 export interface GetCourseResponse extends Course {}
+
+// GET '/course/:id'
+// 해당 API 호출이 필요한 분이 구현해주세요.
+export const getCourse = () => null;
 
 /**
  * 단과대 및 전공 목록 가져오기
@@ -24,3 +32,7 @@ export interface GetCollegesAndMajorsResponse {
   colleges: string[];
   majors: string[];
 }
+
+// GET '/course/colleges-and-majors'
+// 해당 API 호출이 필요한 분이 구현해주세요.
+export const getCollegesAndMajors = () => null;
--- src/apis/mycourse.ts
+++ src/apis/mycourse.ts
@@ -4,18 +4,9 @@ import { HOST_URL } from '@/data/url';
 import { Course } from '@/interfaces/Course';
 
 /**
- * 신청 내역
- */
-export type GetAppliedCoursesRequest = void;
-
-export interface GetAppliedCoursesResponse extends Array<Course> {}
-
-/**
  * 수강 신청
  */
-export interface ApplyRequest {
-  courseId: string;
-}
+export interface ApplyRequest extends Pick<Course, 'id'> {}
 
 export type ApplyResponse = Course[];
 
@@ -31,18 +22,27 @@ export const apply = async (course: ApplyRequest): Promise<ApplyResponse> => {
 /**
  * 수강 취소
  */
-export interface WithdrawRequest {
-  courseId: string;
-}
+export interface WithdrawRequest extends Pick<Course, 'id'> {}
 
 export type WithdrawResponse = Course[];
 
 export const withdraw = async ({
-  courseId,
+  id,
 }: WithdrawRequest): Promise<WithdrawResponse> => {
   const response = await axios.delete<WithdrawResponse>(
-    `${HOST_URL}/course/mycourse/${courseId}`,
+    `${HOST_URL}/course/mycourse/${id}`,
   );
 
   return response.data;
 };
+
+/**
+ * 신청 내역 List
+ */
+export type GetAppliedCoursesRequest = void;
+
+export interface GetAppliedCoursesResponse extends Array<Course> {}
+
+// GET /course/mycourse
+// 해당 API 호출이 필요한 분이 구현해주세요.
+export const getAppliedCourses = () => null;
